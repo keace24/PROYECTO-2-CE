9debbe4cf78cfda8c5f516a062199c17650627fa
Armando <armandofg3937@gmail.com>
2019-05-15 23:35:53 -0600

    Intento de minas

--- game.py
+++ game.py
@@ -1,12 +1,20 @@
-import pygame
+import pygame, sys
 import socket
 #from network import Network
 import pickle
 import tkinter
+from random import randint
+
+global Num_x, Num_y, contador
+Num_x = 0
+Num_y = 0
+contador = 0
 
 
 ancho = 1366
 alto = 768
+
+pygame.init()
 
 class Network():
 
@@ -39,6 +47,53 @@ class Network():
         except socket.error as e:
             return str(e)
 
+class mina(pygame.sprite.Sprite):
+        def __init__(self):
+                pygame.sprite.Sprite.__init__(self)
+                self.imagen_mina = pygame.image.load ("mina.png")
+                self.imagen_mina = pygame.transform.scale(self.imagen_mina,(40,40))
+                self.rect = self.imagen_mina.get_rect()
+                global Num_x, Num_y, contador
+                self.rect.top = Num_x
+                self.rect.left = Num_y
+                self.aparicion_mina = 5
+                self.lista_mina = []
+                self.Num_x = 0
+            
+    
+                
+                
+        # Define los tiempos para llamar las funciones de rango
+        def comportamiento(self, tiempo):
+                #self.Rango()
+                self.Rango_x()
+
+        # Origina numeros al azar para el eje x para que las estrellas aparescan en distintios lugares
+        def Rango_x(self):
+                global Num_x, Num_y
+                Num_x = (randint(0,1200))
+                Num_y = (randint(0,700))
+                
+        
+
+        #Define el tiempo de aparicion de cada una de las estrellas
+
+        #def Rango(self):
+                
+
+        
+                
+        #Agrega las estrellas creadas en una lista        
+        def aparicion(self,x,y):
+                global contador
+                Aparicion = mina()
+                self.lista_mina.append(Aparicion)
+                contador += 1
+                 
+               
+        # Se dibuja en la ventana las estrellas
+        def Dibujar (self, superficie):
+                superficie.blit (self.imagen_mina, self.rect)
 
 
 class Player():
@@ -72,18 +127,25 @@ class Player():
 class Game:
 
     def __init__(self, w, h):
+        pygame.init()
+        self.screen = pygame.display.set_mode((ancho, alto),pygame.FULLSCREEN)
+        pygame.display.set_caption ("pyDakarDeath")
+        
         self.net = Network()
         self.width = w
         self.height = h
         self.player = Player(50, 50)
         self.player2 = Player(200,200)
-        self.canvas = Canvas(self.width, self.height, "Testing...")
+        self.MINAS = mina()
 
     def run(self):
+        global contador
         clock = pygame.time.Clock()
         run = True
         while run:
+            self.screen.fill((255,255,255))
             clock.tick(60)
+            self.tiempo = pygame.time.get_ticks()/1000
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -95,7 +157,7 @@ class Game:
             keys = pygame.key.get_pressed()
 
             if keys[pygame.K_RIGHT]:
-                if self.player.x <= 1245 - self.player.velocity:
+                if self.player.x <= 1320 - self.player.velocity:
                     self.player.move(0)
 
             if keys[pygame.K_LEFT]:
@@ -107,17 +169,30 @@ class Game:
                     self.player.move(2)
 
             if keys[pygame.K_DOWN]:
-                if self.player.y <= 600 - self.player.velocity:
+                if self.player.y <= 723 - self.player.velocity:
                     self.player.move(3)
 
             # Send Network Stuff
             self.player2.x, self.player2.y = self.parse_data(self.send_data())
 
             # Update Canvas
-            self.canvas.draw_background()
-            self.player.draw(self.canvas.get_canvas())
-            self.player2.draw(self.canvas.get_canvas())
-            self.canvas.update()
+            self.player.draw(self.screen)
+            self.player2.draw(self.screen)
+            self.MINAS.comportamiento(self.tiempo)
+
+            if (randint(0,100) == self.MINAS.aparicion_mina):
+                        x = self.MINAS.rect.left
+                        y = self.MINAS.rect.top
+                        self.MINAS.aparicion(x,y)
+                        for x in self.MINAS.lista_mina:
+                                     print("Hola")
+                                     x.Dibujar(self.screen)
+                                    
+           # if len(self.MINAS.lista_mina) > 0:
+                                 
+                                                          
+            pygame.display.update()
+                       
 
         pygame.quit()
 
@@ -139,32 +214,16 @@ class Game:
             return 0,0
 
 
-class Canvas:
 
-    def __init__(self, w, h, name="None"):
-        #Se minimixa la ventana principal
-        #self.Ventana.withdraw()
-        #Se definen el tamaño de la pantalladel juego
-        self.screen = pygame.display.set_mode((ancho, alto),pygame.FULLSCREEN)
-        pygame.display.set_caption ("pyDakarDeath")
 
-    @staticmethod
-    def update():
-        pygame.display.update()
 
-    def draw_text(self, text, size, x, y):
-        pygame.font.init()
-        font = pygame.font.SysFont("comicsans", size)
+   # def draw_text(self, text, size, x, y):
+    #    pygame.font.init()
+     #   font = pygame.font.SysFont("pyDakarDeath", size)
 
-        self.screen.draw(render, (x,y))
+      #  self.screen.draw(render, (x,y))
 
-    def get_canvas(self):
-        pass
-        return self.screen
 
-    def draw_background(self):
-        pass
-        self.screen.fill((255,255,255))
 
 if __name__ == "__main__":
     g = Game(ancho,alto)

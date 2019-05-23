d307298b72a7e7e361c71a69fcae9a9c3cda2f2f
Armando <armandofg3937@gmail.com>
2019-05-22 22:24:10 -0600

    mejora

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 import pygame, sys
 import socket
-##from network import Network
+#from network import Network
 import pickle
 import tkinter
 from random import randint
@@ -9,7 +9,7 @@ import time
 from tkinter import *
 from tkinter import messagebox
 
-global Num_x, Num_y, contador, mina, dibujar_mina, Imagen_Disparo_Jugador, minutos, reply_1, tiempo_pausa, segundos
+global Num_x, Num_y, contador, mina, dibujar_mina, Imagen_Disparo_Jugador, minutos, reply_1, tiempo_pausa, segundos, Jugador1, Jugador2, vida_player, vida_player2
 Num_x = 0
 Num_y = 0
 contador = 0
@@ -20,8 +20,8 @@ minutos = 0
 segundos = 0
 reply_1 = ""
 tiempo_pausa = 0
-Name = ''
-marcador = 0
+vida_player = 100
+vida_player2 = 100
 
 ancho = 1366
 alto = 768
@@ -56,7 +56,6 @@ class Network():
         self.port = 5555            
         self.addr = (self.host, self.port)
         self.id = self.connect()
-        print(self.id)
     def connect(self):
         self.client.connect(self.addr)
         recibir = self.client.recv(2048)
@@ -73,13 +72,13 @@ class Network():
         
         if isinstance(reply_1, list):
             if self.id != self.id:
-                return reply_1
+                datos = reply_1
+
                
                 
         if isinstance(reply_1, str):
-            print (reply_1)
             return reply_1
-     
+        
 
 class mina(pygame.sprite.Sprite):
         def __init__(self):
@@ -121,36 +120,6 @@ class mina(pygame.sprite.Sprite):
         def Dibujar (self, superficie):
                 superficie.blit (self.imagen_mina, self.rect)
 
-class Nombre_Jugador():
-        def __init__(self):
-        
-                global Name, marcador
-                marcador = 0
-                pygame.font.init()
-                self.Canvas_Jugador = Canvas (Ventana, bg = "black", width = 250, height = 300)
-                self.Canvas_Jugador.pack()
-
-                self.dato=tkinter.StringVar()
-                self.Nombre = Entry(self.Canvas_Jugador, bd = 5, justify = LEFT, textvariable=self.dato)
-                self.Nombre.place(x=120,y=20)
-                
-                self.Label_Jugador = Label (self.Canvas_Jugador,text = "Jugador:",fg = "white",bg = "black")
-                self.Label_Jugador.place (x=20,y=20)
-                
-                self.B_Guardar = tkinter.Button(self.Canvas_Jugador, text="Guardar",fg="white",width=9,height=2,bg="GREEN",command=self.Lista_J, cursor='hand2')
-                self.B_Guardar.place(x=120,y=50)
-                self.B_Jugar2 = tkinter.Button(self.Canvas_Jugador, text="Jugar",fg="white",width=9,height=2,bg="GREEN",command=Game, cursor='hand2')
-                self.B_Jugar2.place(x=120,y=100)
-
-        def Lista_J(self):
-                global Name
-                archivo = open ("Jugadores.csv","a")
-                Name = self.dato.get()
-                archivo.write(Name)
-                archivo.write("\n")
-                print (Name)
-                archivo.close
-
 class Proyectil(pygame.sprite.Sprite):
         def __init__(self,posx,posy, imagen, personaje):
                 pygame.sprite.Sprite.__init__(self)
@@ -190,6 +159,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = starty
         self.velocity = 2
         self.lista_disparo = []
+        
     def Disparar (self,x,y):
         
                 global Imagen_Disparo_Jugador
@@ -229,6 +199,8 @@ class Game:
                 segundos += 1
                 time.sleep(1)
                 return Crono()
+        hilo = threading.Thread(target = Crono, args = ())
+        hilo.start()
         pygame.init()
         self.screen = pygame.display.set_mode((ancho, alto),pygame.FULLSCREEN)
         pygame.display.set_caption ("pyDakarDeath")
@@ -240,12 +212,12 @@ class Game:
         self.player = Player(590, 700)
         self.player2 = Player(690,700)
         self.MINAS = mina()
-        hilo = threading.Thread(target = Crono, args = ())
-        hilo.start()
-
-    
+         
+                                
+                                       
+        
     def pausa(self):
-                global tiempo_pausa, segundos
+                global tiempo_pausa, segundos, Jugador2
                 pygame.font.init()
                 Texto_Pausa = pygame.font.Font (None, 45)
                 Texto_Pausa = Texto_Pausa.render("Juego Pausado ", 0,(255,255,255))
@@ -261,16 +233,16 @@ class Game:
                       #  pygame.display.update()
     
     def run(self):
-        global contador, minutos, segundos
+        global contador, minutos, segundos, Jugador1, Jugador2, vida_player, vida_player2
         clock = pygame.time.Clock()
         run = True
+        #self.Esperando_Jugadores()
         while run:
             
             self.screen.fill((255,255,255))
             clock.tick(60)
             self.tiempo = pygame.time.get_ticks()/1000
             
-
             Texto_puntaje = pygame.font.Font (None, 50)
             Texto_Pantalla = Texto_puntaje.render("Tiempo " + str(minutos) + " : " + str(int(segundos)), 0,(255,255,255))
 
@@ -324,7 +296,7 @@ class Game:
                     self.player.move(3)
 
             # Send Network Stuff
-            self.player2.rect.x, self.player2.rect.y = self.parse_data(self.send_data())
+            self.player2.rect.x, self.player2.rect.y, segundos, minutos, vida_player2 = self.parse_data(self.send_data())
 
             # Update Canvas
             self.screen.blit(self.fondo, (0, 0))
@@ -342,7 +314,7 @@ class Game:
                         values=[self.net.id, "Mina", Num_x, Num_y, dibujar_mina]
                         reply2 = self.net.send(values)
                         return reply2
-                          """ 
+                """
             if len(self.player.lista_disparo) > 0:
                     for x in self.player.lista_disparo:
                         x.Dibujar(self.screen)
@@ -350,14 +322,21 @@ class Game:
                         if x.rect.top < -20:
                                 self.player.lista_disparo.remove(x)
 
+                        else:
+                                if x.rect.colliderect(self.player2.rect):
+                                                vida_player -= 5
+                                                print('hola')
+                                                print(vida_player)
+                                                self.player.lista_disparo.remove(x)
+            #if vida_player == 0:
+                    
                                 
            # if len(self.MINAS.lista_mina) > 0:
             if len(self.MINAS.lista_mina) > 0:
                 for x in self.MINAS.lista_mina:
                                      x.Dibujar(self.screen)
                                      
-            self.screen.blit(Texto_Pantalla,(20,20))
-                    
+            self.screen.blit(Texto_Pantalla,(20,20))     
             pygame.display.update()
             pygame.display.flip()           
 
@@ -365,7 +344,7 @@ class Game:
 
     def send_data(self):
 
-        data = str(self.net.id) + ":" + str(self.player.rect.x) + "," + str(self.player.rect.y)
+        data = str(self.net.id) + ":" + str(self.player.rect.x) + "," + str(self.player.rect.y) + "," + str(segundos) + "," + str(minutos) + "," + str(vida_player)
         reply = self.net.send(data)
         return reply
 
@@ -373,11 +352,9 @@ class Game:
     def parse_data(data):
         try:
             d = data.split(":")[1].split(",")
-            return int(d[0]), int(d[1])
+            return int(d[0]), int(d[1]), int(d[2]), int(d[3]), int(d[4])
         except:
             return 0,0
-
-
 
 def iniciar():
 
@@ -388,8 +365,6 @@ def iniciar():
 B_Salir = tkinter.Button(Ventana, text="Salir",fg="white",width=10,height=3,bg="GREEN",command=Salir, cursor='pirate')
 B_Salir.place(x=642,y=600)
 B_Jugar = tkinter.Button(Ventana, text="Jugar",fg="white",width=10,height=3,bg="GREEN",command=iniciar, cursor='hand2')
-B_Jugar.place(x=642,y=540)
-B_Jugar = tkinter.Button(Ventana, text="Jugar",fg="white",width=10,height=3,bg="GREEN",command=Nombre_Jugador, cursor='hand2')
 B_Jugar.place(x=642,y=540)
 
 
